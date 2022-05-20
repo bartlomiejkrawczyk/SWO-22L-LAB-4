86478509b889d55781a472bfb4b0a71adb1e8d4c
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-05-20 19:59:49 +0200

    Code Smell: remove unusued strings in WhiteBear.h

--- src/WhiteBear.h
+++ src/WhiteBear.h
@@ -17,7 +17,3 @@ class WhiteBear {
    private:
     std::vector<ItemPointer> items_;
 };
-
-const std::string item1 = "Aged Cheese";
-const std::string item2 = "Tickets to a concert";
-const std::string item3 = "Legolas, Hand of Gollum";
