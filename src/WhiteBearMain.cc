605130d9f859cb6410bcd4e2f3b23d22db0e624d
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-05-20 20:20:30 +0200

    Code smell: cleanup main()
    `using namespace std;` is not really
    good practice and can shadow over
    existing names.
    
    And a lonesome HAHAHAHA print
    was also removed

--- src/WhiteBearMain.cc
+++ src/WhiteBearMain.cc
@@ -1,14 +1,11 @@
 #include "WhiteBear.h"
 #include "NormalItem.h"
 
-using namespace std;
-
 int main() {
     WhiteBear app;
 
     app.addItem(new NormalItem("+7 Yellow Vest", 10, 20));
 
-    cout << "HAHAHA!" << endl;
     app.updateQuality();
-    app.printItems(cout);
+    app.printItems(std::cout);
 }
