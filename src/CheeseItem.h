d511c1704d3e53c9ed5e7400d2d393a4c0f70e4b
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-05-20 19:22:25 +0200

    Create a class representing Cheese

---
+++ src/CheeseItem.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "IItem.h"
+
+class CheeseItem : public IItem {
+   public:
+    CheeseItem(const std::string& name, int days_remaining, int quality)
+        : IItem(name, days_remaining, quality) {}
+    virtual ~CheeseItem() = default;
+
+    void update() override {
+        const int quality_increment = (getDaysRemaining() <= 0) ? 2 : 1;
+
+        incrementQualityBy(quality_increment);
+        decreaseDaysRemaining();
+    }
+};
